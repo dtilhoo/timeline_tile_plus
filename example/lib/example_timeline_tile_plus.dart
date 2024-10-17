123b0baf41fa7dbee18700a23086912827af5abf
Dheeraj TILHOO <d.tilhoo@xefi.fr>
2024-10-17 08:46:36 +0200

    initial commit

---
+++ example/lib/example_timeline_tile_plus.dart
@@ -0,0 +1,3 @@
+library example_timeline_tile_plus;
+
+export 'package:example_timeline_tile_plus/src/showcase_timeline_tile_plus.dart';
---
+++ lib/timeline_tile_plus.dart
@@ -0,0 +1,5 @@
+library timeline_tile_plus;
+
+export 'package:timeline_tile_plus/src/tile.dart';
+export 'package:timeline_tile_plus/src/style.dart';
+export 'package:timeline_tile_plus/src/timeline_divider.dart';
